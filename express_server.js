5b88fa0b16e4fea51829710829a658388d3e9779
Ibirn <kmcricciuti@gmail.com>
2020-08-31 16:14:27 +0000

    added html tags on new hello page

--- express_server.js
+++ express_server.js
@@ -6,11 +6,19 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+//can contain html in res.send
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Heck");
 })
 
 app.listen(PORT, () => {
   console.log(`Example app listnening on port: ${PORT}`)
+})
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase)
+})
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <strong>World!</strong></body></hmtl>\n");
 })
