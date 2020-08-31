d44b98f4fa525f2640898bbf09f450eaf21db49b
Ibirn <kmcricciuti@gmail.com>
2020-08-31 19:52:38 +0000

    some linting and a test with variables done

--- express_server.js
+++ express_server.js
@@ -6,19 +6,31 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 //can contain html in res.send
 app.get("/", (req, res) => {
   res.send("Heck");
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listnening on port: ${PORT}`)
-})
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase)
-})
+});
+
+//curl -i localhost:8080/hello to see headers and html:
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <strong>World!</strong></body></hmtl>\n");
-})
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`)
+});
+
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`)
+});
