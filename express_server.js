f10d57e5aa9761b4892acee4fbc2a0bb7d546849
Ibirn <kmcricciuti@gmail.com>
2020-09-04 04:15:38 +0000

    added comments identifying blocks of code, emptied trial data from databases.

--- express_server.js
+++ express_server.js
@@ -2,57 +2,45 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
-const { urlsForID, getID, checkInUse, genRandomString } = require("./helpers")
+const { urlsForID, getID, checkInUse, genRandomString } = require("./helpers");
 const cookieSession = require("cookie-session");
 const bcrypt = require('bcrypt');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
   name: 'session',
-  keys: ['I am the very model of a scientist salarian',"I've studies species Turian, Asari, and Batarian","I'm quite good at genetics(as a subset of biology", "because I am a expert (which I know is a tautology)" ]
+  keys: ['I am the very model of a scientist salarian', "I've studies species Turian, Asari, and Batarian", "I'm quite good at genetics(as a subset of biology)", "because I am a expert (which I know is a tautology)"]
 }));
 
 //DATABASES------------------------------------------------------------------------
 
-const urlDatabase = {
-  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
-  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
-};
+const urlDatabase = {};
 
-const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
-  },
-  "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
-};
+const users = {};
 
 const viewCount = {};
 
 const errorLog = {
-  400: {id: "Error 400", msg:"Bad request."},
-  401: {id: "Error 401", msg:"You do not have permission to edit this link."},
-  403: {id: "Error 403", msg:"You do not have authorization."},
-  404: {id: "Error 404", msg:"Not here, chief."},
+  400: { id: "Error 400", msg: "Bad request." },
+  401: { id: "Error 401", msg: "You do not have permission to edit this link." },
+  403: { id: "Error 403", msg: "You do not have authorization." },
+  404: { id: "Error 404", msg: "Not here, chief." },
 
-  3: {id: "E-mail already registered", msg:"We're already friends, bud."},
-  2: {id: "Bad password/email combination.", msg:"Please double-check your spelling."},
-  1: {id: '', msg:"We know you want to get places, but please log in or register first."}
+  3: { id: "E-mail already registered", msg: "We're already friends, bud." },
+  2: { id: "Bad password/email combination.", msg: "Please double-check your spelling." },
+  1: { id: '', msg: "We know you want to get places, but please log in or register first." },
+  0: { id: "Hey, how'd you get here?", msg: "Try logging in or register to use TinyApp!" }
 };
-
 
 //PRODUCTION CODE-------------------------------------------------------------
 
 app.set('view engine', 'ejs');
 
+//Check port is open:
 app.listen(PORT, () => {
-  console.log(`The MIRROR DIMENSION has opened on port: ${PORT}`);
+  console.log(`A SERIES OF TUBES has opened on port: ${PORT}`);
 });
 
+//Root:
 app.get("/", (req, res) => {
   if (users[req.session.user_id]) {
     res.redirect("/urls");
@@ -61,16 +49,22 @@ app.get("/", (req, res) => {
   }
 });
 
+//Index page for listing tinyURLs:
 app.get("/urls", (req, res) => {
   if (users[req.session.user_id]) {
-    let templateVar = { urls: urlsForID(req.session.user_id, urlDatabase), user: users[req.session.user_id], viewCount };
+    let templateVar = {
+      urls: urlsForID(req.session.user_id, urlDatabase),
+      user: users[req.session.user_id], 
+      viewCount
+    };
     res.render('urls_index', templateVar);
   } else {
     req.session.errorLog = 1;
-    res.redirect("/error");
+    res.redirect(`/error/${req.session.errorLog}`);
   }
 });
 
+//tinyURL creation:
 app.get("/urls/new", (req, res) => {
   if (!users[req.session.user_id]) {
     return res.redirect("/login");
@@ -79,11 +73,16 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVar);
 });
 
+//Error redirect base:
 app.get("/error", (req, res) => {
-  let templateVar = { user: users[req.session.user_id], error:errorLog[req.session.errorLog]};
+  let templateVar = {
+    user: users[req.session.user_id],
+    error: errorLog["0"]
+  };
   res.render("urls_error", templateVar);
 });
 
+//Registration page:
 app.get("/register", (req, res) => {
   if (users[req.session.user_id]) {
     return res.redirect("/urls");
@@ -93,6 +92,7 @@ app.get("/register", (req, res) => {
   }
 });
 
+//Login page:
 app.get("/login", (req, res) => {
   if (users[req.session.user_id]) {
     return res.redirect("/urls");
@@ -102,31 +102,49 @@ app.get("/login", (req, res) => {
   }
 });
 
+//TinyURL showpage including original URL and tinyURL for copying, as well as form to edit tinyURL link:
 app.get("/urls/:shortURL", (req, res) => {
   req.params;
   if (!urlDatabase[req.params.shortURL]) {
     req.session.errorLog = 404;
-    res.redirect("/error");
+    res.redirect(`/error/${req.session.errorLog}`);
   } else if (req.session.user_id === urlDatabase[req.params.shortURL]["userID"]) {
-    let templateVar = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]["longURL"], user: users[req.session.user_id], date: urlDatabase[req.params.shortURL]["date"]};
+    let templateVar = {
+      shortURL: req.params.shortURL,
+      longURL: urlDatabase[req.params.shortURL]["longURL"],
+      user: users[req.session.user_id],
+      date: urlDatabase[req.params.shortURL]["date"]
+    };
     req.params;
     res.render("urls_show", templateVar);
   } else {
     req.session.errorLog = 403;
-    res.redirect("/error");
+    res.redirect(`/error/${req.session.errorLog}`);
   }
 });
 
+//Error page redirects:
+app.get("/error/:errorlog", (req, res) => {
+  req.params;
+  let templateVar = {
+    user: users[req.session.user_id],
+    error: errorLog[req.params.errorlog]
+  };
+  res.render("urls_error", templateVar);
+});
+
+//Immediate tinyURL redirects:
 app.get("/u/:shortURL", (req, res) => {
   req.params;
   if (!urlDatabase[req.params.shortURL]) {
     req.session.errorLog = 404;
-    res.redirect("/error");
+    res.redirect(`/error/${req.session.errorLog}`);
   }
   viewCount[req.params.shortURL] += 1;
   res.redirect(urlDatabase[req.params.shortURL]["longURL"]);
 });
 
+//Submit login credentials:
 app.post("/login", (req, res) => {
   if (checkInUse(req.body.email, users)) {
     let check = getID(req.body.email, req.body.password, users);
@@ -135,28 +153,30 @@ app.post("/login", (req, res) => {
       return res.redirect("urls");
     }
     req.session.errorLog = 2;
-    res.redirect("/error");
+    res.redirect(`/error/${req.session.errorLog}`);
   } else {
     req.session.errorLog = 2;
-    res.redirect("/error");
+    res.redirect(`/error/${req.session.errorLog}`);
   }
 });
 
+//Logout and clear cookies:
 app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect("/urls");
 });
 
+//Submit registration credentials:
 app.post("/register", (req, res) => {
   let temp = genRandomString(urlDatabase);
   req.session.user_id = temp;
   req.body;
   if (req.body.email === '' || req.body.password === '') {
     req.session.errorLog = 400;
-    res.redirect("/error");
+    res.redirect(`/error/${req.session.errorLog}`);
   } else if (checkInUse(req.body.email, users)) {
     req.session.errorLog = 3;
-    res.redirect("/error");
+    res.redirect(`/error/${req.session.errorLog}`);
   } else {
     users[temp] = {
       id: temp,
@@ -167,22 +187,28 @@ app.post("/register", (req, res) => {
   }
 });
 
+//Generates tinyURL
 app.post("/urls", (req, res) => {
-  let temp = genRandomString(urlDatabase);
-  let date= new Date()
-  if (!(req.body.longURL.slice(0,7) === 'http://')) {
-    const format = 'http://';
-    req.body.longURL = format.concat(req.body.longURL);
+  if (users[req.session.user_id]) {
+    let temp = genRandomString(urlDatabase);
+    let date = new Date();
+    if (req.body.longURL.slice(0, 7) !== 'http://' && req.body.longURL.slice(0,8) !== 'https://') {
+      const format = 'https://';
+      req.body.longURL = format.concat(req.body.longURL);
+    }
+    urlDatabase[temp] = {
+      longURL: req.body.longURL,
+      userID: req.session.user_id,
+      date: date
+    };
+    viewCount[temp] = 0;
+    res.redirect(`/urls/${temp}`);
+  } else {
+    res.redirect(401, `/error/${errorLog[401]}`);
   }
-  urlDatabase[temp] = {
-    longURL: req.body.longURL,
-    userID: req.session.user_id,
-    date: date
-  };
-  viewCount[temp] = 0;
-  res.redirect(`/urls/${temp}`);
 });
 
+//Updates where the tinyURL links to:
 app.post("/urls/:shortURL", (req, res) => {
   req.params;
   if (req.session.user_id === urlDatabase[req.params.shortURL]["userID"]) {
@@ -191,18 +217,17 @@ app.post("/urls/:shortURL", (req, res) => {
     res.redirect("/urls");
   } else {
     req.session.errorLog = 401;
-    res.redirect("/error");
+    res.redirect(`/error/${req.session.errorLog}`);
   }
 });
 
+//Deletes tinyURL:
 app.post("/urls/:shortURL/delete", (req, res) => {
   req.params;
   if (req.session.user_id === urlDatabase[req.params.shortURL]["userID"]) {
     delete urlDatabase[req.params.shortURL];
     res.redirect('/urls');
   } else if (req.session.user_id !== urlDatabase[req.params.shortURL]["userID"]) {
-    res.send(401)
-    req.session.errorLog = 401;
-    res.redirect("/error");
+    res.redirect(401, `/error/${errorLog[401]}`);
   }
 });
